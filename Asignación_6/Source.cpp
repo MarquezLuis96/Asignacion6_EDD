7fe53195114a02ad54d27a9d132b0a699ffc42cd
Luis Marquez <englamc@gmail.com>
2022-02-02 11:46:17 -0400

    From str to long in doubly linked list

--- Asignación_6/Source.cpp
+++ Asignación_6/Source.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+
 
 using namespace std;
 
@@ -38,9 +40,118 @@ struct Node {
 		}
 };
 
-int main(int args, char* argsv[]) {
+template <class dataType>
+class DoubleLinkedList {
+	private:
+		Node<dataType>* sentinel;
+	public:
+		DoubleLinkedList() {
+			sentinel = new Node<dataType>;
+			sentinel->setNext(sentinel);
+			sentinel->setPrevious(sentinel);
+		}
 
-	 Node<int> n1, n2, n3, nNULL;
+		void addNode(Node<dataType>* toAdd) {
+			toAdd->setNext(sentinel->getNext());
+			sentinel->getNext()->setPrevious(toAdd);
+			sentinel->setNext(toAdd);
+			toAdd->setPrevious(sentinel);
+		}
+
+		Node<dataType>* getSentinel() {
+			return sentinel;
+		}
+
+		void printList() {
+			int i = 0;
+			Node<dataType>* currentNode = sentinel->getNext();
+			while (currentNode != sentinel) {
+				cout << "N" << ++i << " = " << currentNode->getKey() << endl;
+				currentNode = currentNode->getNext();
+			}
+
+		}
+
+		void printListOrdered() {
+			int i = 0;
+			Node<dataType>* currentNode = sentinel->getPrevious();
+			while (currentNode != sentinel) {
+				cout << "N" << ++i << " = " << currentNode->getKey() << endl;
+				currentNode = currentNode->getPrevious();
+			}
+
+		}
+
+		void agregarNodo(int i) {
+			struct Node<int>* p;
+			p = (Node<int>*) malloc(sizeof(struct Node<int>));
+			p->setKey(i);
+			p->setNext(sentinel);
+			p->setPrevious(sentinel->getPrevious());
+			sentinel->getPrevious()->setNext(p);
+			sentinel->setPrevious(p);
+		}
+
+		void transformStrToList() {
+			string str;
+			cout << "Ingrese un numero" << endl;
+			cin >> str;
+			cout << "El length del numero es de: " << str.length() << endl;
+			for (int i = 0; i < str.length(); i++) {
+				int key = (str[i] - '0');
+				agregarNodo(key);
+			}
+
+			printListOrdered();
+		}
+
+};
+
+int main(int args, char* argsv[]) {
+	DoubleLinkedList<int> list;
+	list.transformStrToList();
+	/*Node<int> n1, n2, n3, n4, n5, n6;
+
+	n1.setKey(1);
+	list.addNode(&n1);
+
+	n2.setKey(2);
+	list.addNode(&n2);
+
+	n3.setKey(3);
+	list.addNode(&n3);
+
+	list.printListOrdered();*/
+	/*n2.setKey(2);
+	n3.setKey(3);
+
+	list.addNode(&n1);
+	list.addNode(&n2);
+	list.addNode(&n3);
+
+	n4.setKey(4);
+	n5.setKey(5);
+	n6.setKey(6);
+
+	list.addNode(&n4);
+	list.addNode(&n5);
+	list.addNode(&n6);
+
+	cout << "Printing inverse" << endl;
+	list.printList();
+	cout << endl;
+	cout << endl;
+	cout << "Printing in order" << endl;
+	list.printListOrdered();
+	cout << endl;
+	cout << endl;*/
+
+	/*cout << "Start: " << list.getSentinel()->getKey() << endl;
+	cout << "N1: " << list.getSentinel()->getNext()->getKey() << endl;
+	cout << "N2: " << list.getSentinel()->getNext()->getNext()->getKey() << endl;*/
+
+	/*Node Class Try*/
+	 /*Node<int> n1, n2, n3, nNULL;
 
 	 n1.setKey(1);
 	 n2.setKey(2);
@@ -77,7 +188,7 @@ int main(int args, char* argsv[]) {
 
 	 cout << "Next 1: " << n1.getNext()->getKey() << endl;
 	 cout << "Next 2: " << n2.getNext()->getKey() << endl;
-	 cout << "Next 3: " << n3.getNext()->getKey() << endl;
+	 cout << "Next 3: " << n3.getNext()->getKey() << endl;*/
 
 	return 0;
 }
