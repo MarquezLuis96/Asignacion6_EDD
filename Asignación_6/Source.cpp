0a6ff4dd9ca17a1afd2529ed3e77fe6bda76bc9b
Luis Marquez <englamc@gmail.com>
2022-02-01 20:14:55 -0400

    Adding Node Class

--- Asignación_6/Source.cpp
+++ Asignación_6/Source.cpp
@@ -2,6 +2,42 @@
 
 using namespace std;
 
+template <class dataType>
+struct Node {
+	private:
+		Node* previous;
+		Node* next;
+		dataType key;
+	public:
+		Node() {
+			previous = next = NULL;
+		}
+
+		void setKey(dataType key) {
+			this->key = key;
+		}
+
+		dataType getKey() {
+			return this->key;
+		}
+
+		void setPrevious(Node previous) {
+			this->previous = previous;
+		}
+
+		Node getPrevious() {
+			return this->previous;
+		}
+
+		void setNext(Node next) {
+			this->next = next;
+		}
+
+		Node getNext() {
+			return this->next;
+		}
+};
+
 int main(int args, char* argsv[]) {
 	cout << "Hola Mundo!" << endl;
 	return 0;
