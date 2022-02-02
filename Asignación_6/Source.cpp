900a4e0be785a1e36fc796bfa391497bea21935f
Luis Marquez <englamc@gmail.com>
2022-02-02 17:28:57 -0400

    Listo Calixto

--- Asignación_6/Source.cpp
+++ Asignación_6/Source.cpp
@@ -82,6 +82,16 @@ class DoubleLinkedList {
 
 		}
 
+		void printListOrderedInline() {
+			int i = 0;
+			Node<dataType>* currentNode = sentinel->getPrevious();
+			while (currentNode != sentinel) {
+				cout << currentNode->getKey();
+				currentNode = currentNode->getPrevious();
+			}
+			cout << endl;
+		}
+
 		void agregarNodo(int i) {
 			struct Node<int>* p;
 			p = (Node<int>*) malloc(sizeof(struct Node<int>));
@@ -94,101 +104,124 @@ class DoubleLinkedList {
 
 		void transformStrToList() {
 			string str;
-			cout << "Ingrese un numero" << endl;
 			cin >> str;
-			cout << "El length del numero es de: " << str.length() << endl;
 			for (int i = 0; i < str.length(); i++) {
 				int key = (str[i] - '0');
 				agregarNodo(key);
 			}
+		}
 
-			printListOrdered();
+		int contarDigitos() {
+			int i = 0;
+			Node<dataType>* currentNode = sentinel->getPrevious();
+			while (currentNode != sentinel) {
+				++i;
+				currentNode = currentNode->getPrevious();
+			}
+			return i;
 		}
 
 };
 
+void sumarLongs(DoubleLinkedList<int> &l1, DoubleLinkedList<int>& l2) {
+	int llevo, anoto;
+	int aux1, aux2, auxR;
+	bool llevoPendiente = false;
+	DoubleLinkedList<int> listaResultado;
+	Node<int>* currentN1 = l1.getSentinel()->getPrevious();
+	Node<int>* currentN2 = l2.getSentinel()->getPrevious();
+	while (currentN2 != l2.getSentinel()) {
+		
+		auxR = currentN1->getKey() + currentN2->getKey();
+		
+		if (llevoPendiente) {
+			auxR += llevo;
+		}
+
+		if (auxR < 10) {
+			listaResultado.agregarNodo(auxR);
+			llevoPendiente = false;
+		}
+		else {
+			llevoPendiente = true;
+			llevo = auxR / 10;
+			anoto = auxR % 10;
+			listaResultado.agregarNodo(anoto);
+		}
+		
+		currentN1 = currentN1->getPrevious();
+		currentN2 = currentN2->getPrevious();
+	}
+
+	while (currentN1 != l1.getSentinel()) {
+		if (llevoPendiente) {
+			auxR = llevo + currentN1->getKey();
+		}
+		else {
+			listaResultado.agregarNodo(currentN1->getKey());
+			currentN1 = currentN1->getPrevious();
+			continue;
+		}
+
+		if (auxR < 10) {
+			listaResultado.agregarNodo(auxR);
+			llevoPendiente = false;
+		}
+		else {
+			llevoPendiente = true;
+			llevo = auxR / 10;
+			anoto = auxR % 10;
+			listaResultado.agregarNodo(anoto);
+		}
+
+		currentN1 = currentN1->getPrevious();
+	}
+
+	if (llevoPendiente) {
+		listaResultado.agregarNodo(llevo);
+	}
+
+	cout << endl << endl << "Result" << endl;
+	listaResultado.printListOrderedInline();
+
+}
+
 int main(int args, char* argsv[]) {
-	DoubleLinkedList<int> list;
-	list.transformStrToList();
-	/*Node<int> n1, n2, n3, n4, n5, n6;
+	int opc = 1;
 
-	n1.setKey(1);
-	list.addNode(&n1);
+	do {
+		cout << "Ingrese:" << endl;
+		cout << "1 - Para realizar una suma con enteros Long" << endl;
+		cout << "0 - Para salir" << endl;
+		cin >> opc;
 
-	n2.setKey(2);
-	list.addNode(&n2);
+		if (opc == 0) {
+			cout << "Hasta Luego... " << endl;
+			break;
+		}
 
-	n3.setKey(3);
-	list.addNode(&n3);
+		if (opc == 1) {
+			DoubleLinkedList<int> list1;
+			DoubleLinkedList<int> list2;
+			cout << endl << "Ingrese el primer operando:" << endl;
+			list1.transformStrToList();
 
-	list.printListOrdered();*/
-	/*n2.setKey(2);
-	n3.setKey(3);
+			cout << endl << "Ingrese el segundo operando:" << endl;
+			list2.transformStrToList();
 
-	list.addNode(&n1);
-	list.addNode(&n2);
-	list.addNode(&n3);
-
-	n4.setKey(4);
-	n5.setKey(5);
-	n6.setKey(6);
-
-	list.addNode(&n4);
-	list.addNode(&n5);
-	list.addNode(&n6);
-
-	cout << "Printing inverse" << endl;
-	list.printList();
-	cout << endl;
-	cout << endl;
-	cout << "Printing in order" << endl;
-	list.printListOrdered();
-	cout << endl;
-	cout << endl;*/
-
-	/*cout << "Start: " << list.getSentinel()->getKey() << endl;
-	cout << "N1: " << list.getSentinel()->getNext()->getKey() << endl;
-	cout << "N2: " << list.getSentinel()->getNext()->getNext()->getKey() << endl;*/
-
-	/*Node Class Try*/
-	 /*Node<int> n1, n2, n3, nNULL;
-
-	 n1.setKey(1);
-	 n2.setKey(2);
-	 n3.setKey(3);
-	 nNULL.setKey(0);
-
-	 n1.setNext(&n2);
-	 n2.setNext(&n3);
-	 n3.setNext(&nNULL);
-
-	 n1.setPrevious(&nNULL);
-	 n2.setPrevious(&n1);
-	 n3.setPrevious(&n2);
-	 
-	 cout << endl;
-	 cout << "Taking the key values:" << endl;
-	 cout << endl;
-
-	 cout << "Key 1: " << n1.getKey() << endl;
-	 cout << "Key 2: " << n2.getKey() << endl;
-	 cout << "Key 3: " << n3.getKey() << endl;
-
-	 cout << endl; 
-	 cout << "Taking the previous values:" << endl;
-	 cout << endl;
-
-	 cout << "Prev 1: " << n1.getPrevious()->getKey() << endl;
-	 cout << "Prev 2: " << n2.getPrevious()->getKey() << endl;
-	 cout << "Prev 3: " << n3.getPrevious()->getKey() << endl;
-
-	 cout << endl;
-	 cout << "Taking the next values" << endl;
-	 cout << endl;
-
-	 cout << "Next 1: " << n1.getNext()->getKey() << endl;
-	 cout << "Next 2: " << n2.getNext()->getKey() << endl;
-	 cout << "Next 3: " << n3.getNext()->getKey() << endl;*/
+			if (list1.contarDigitos() >= list2.contarDigitos()) {
+				sumarLongs(list1, list2);
+				cout << endl;
+			}
+			else {
+				sumarLongs(list2, list1);
+				cout << endl;
+			}
+		}
+		else {
+			cout << "Error: Opcion invalida... Intente de nuevo" << endl;
+		}
+	} while (opc != 0 );
 
 	return 0;
 }
